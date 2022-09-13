1e1fa2dc5f053397603c50ef5315eaaa8edd131c
marcosGO1970 <marcosgarciaoliver@yahoo.com.ar>
2022-09-13 17:30:49 -0300

    Primer commit modelos users y products

---
+++ src/database/migrations/20220913201855-create-product.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      stock_min: {
+        type: Sequelize.INTEGER
+      },
+      stock_max: {
+        type: Sequelize.INTEGER
+      },
+      bodegasId: {
+        type: Sequelize.INTEGER
+      },
+      category: {
+        type: Sequelize.INTEGER
+      },
+      discount: {
+        type: Sequelize.INTEGER
+      },
+      varietalId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};

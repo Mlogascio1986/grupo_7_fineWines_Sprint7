e9c561d0069cfe7f431e59d1da22da0864ba43a3
marcosGO1970 <marcosgarciaoliver@yahoo.com.ar>
2022-09-28 07:56:46 -0300

    Bugs

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 module.exports = {
-  
-  "development" : {
+  "production" : {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
@@ -9,13 +8,14 @@ module.exports = {
   port: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT
   },
+  /*"development" : {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT
+  },*/
 
-  "production" : {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
-    }
+  
 }
